7b9a1016a7995f087fff7a34633c96b506e12a06
justabob1004 <trandafilovpetruha@gmail.com>
2023-03-15 19:26:35 +0200

    2. Массив предназначен для хранения значений роста двенадцати человек. С помощью датчика случайных чисел заполнить массив целыми значениями, лежащими в диапазоне от 163 до 190 включительно.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,11 +1,11 @@
-﻿int[] array = new int[6];
-for (int i = 0; i < 6; i++)
+﻿byte n = byte.Parse(Console.ReadLine());
+double[] array = new double[n];
+Random rnd = new Random();
+for (int i = 0; i < array.Length; i++)
 {
-   Console.Write($"array[{i}]=");
-    array[i] = int.Parse(Console.ReadLine());
-}
-for (int i = 0; i < 6; i++)
-{
+    array[i] = -50 + (50 - (-50)) * rnd.NextDouble();
     Console.WriteLine($"array[{i}]={array[i]}");
 }
+Console.WriteLine();
+Console.WriteLine($"array[0]={array[0]}\t array[{n - 1}]={array[n - 1]}");
 Console.Read();
